80f21ea90334977710e566d26c4b26b33f334140
StepanAndrosov <stepan.news@yandex.ru>
2023-05-06 13:20:12 +0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import  express, {Request, Response} from 'express'
+
+export const app = express()
+const port = 3000
+
+const jsonBody = express.json()
+app.use(jsonBody)
+
+const db = {
+    courses: [
+        {
+            id: 1,
+            title: 'front'
+        },
+        {
+            id: 2,
+            title: 'back'
+        }
+    ]
+}
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.get('/courses', (req: Request, res: Response) => {
+    res.json(db.courses)
+    res.sendStatus(200)
+})
+
+app.post('/courses', (req: Request, res: Response) => {
+    const createdCourse = {
+        id: +(new Date),
+        title: req.body.title
+    }
+    db.courses.push(createdCourse)
+    res.json(createdCourse)
+    res.sendStatus(200)
+})
+
+app.delete('/__test__/', (req: any, res: any) => {
+    db.courses = []
+    res.sendStatus(204)
+})
+
+app.listen(port, () => {
+    console.log(`Exsample app litsening on port ${port}`)
+})
